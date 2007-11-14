c0575a23a88c9c28b689f70bfaf79dd839db702f
csilvers <csilvers@21bedea4-f223-4c8b-73d6-85019ffb75a9>
2007-11-14 00:42:07 +0000

    Tue Nov 13 15:15:46 2007  Google Inc. <opensource@google.com>
    	* sparsehash: version 1.0 release!  We are now out of beta.
    	* Clean up Makefile awk script to be more readable (csilvers)
    	* Namespace fixes: use fewer #defines, move typedefs into namespace

--- trunk/src/windows/config.h
+++ trunk/src/windows/config.h
@@ -109,6 +109,9 @@
    your system. */
 #undef PTHREAD_CREATE_JOINABLE
 
+/* The system-provided hash function. */
+#define SPARSEHASH_HASH  HASH_NAMESPACE::hash_compare
+
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS  1
 
